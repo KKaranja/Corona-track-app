d0b7d364aaee249c388bb8b26968678213e32fbb
isaac karanja <isaackimura@gmail.com>
2020-04-27 21:09:40 +0300

    Updates

---
+++ precache-manifest.9c4388f58ca053aeb88f0e43b1e72c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2589dc53b78ecb9f1c959bdaf5b3f2a",
+    "url": "/Corona-track-app/index.html"
+  },
+  {
+    "revision": "ac66ed0dfa84244fe3ae",
+    "url": "/Corona-track-app/static/css/main.db612fd2.chunk.css"
+  },
+  {
+    "revision": "ceb361ce754fa6624a94",
+    "url": "/Corona-track-app/static/js/2.8826e06c.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/Corona-track-app/static/js/2.8826e06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac66ed0dfa84244fe3ae",
+    "url": "/Corona-track-app/static/js/main.c70b4291.chunk.js"
+  },
+  {
+    "revision": "e34c43e6dc1882318e54",
+    "url": "/Corona-track-app/static/js/runtime-main.887fc6b8.js"
+  }
+]);
